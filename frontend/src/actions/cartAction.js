6d1660302fa2a2fbb2a0f9b3740ad3608de065b5
Артур Галеев <a.galeev@tapir.ws>
2020-10-22 00:39:55 +0500

    Adding The Shopping Cart

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 import {
-  CART_ADD_ITEM
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM
 } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -17,6 +18,15 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       countInStock: data.countInStock,
       qty
     }
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
   })
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
