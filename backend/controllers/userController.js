86d1f77e081ca022a5f0718db06c6d60e53a21e7
Артур Галеев <a.galeev@tapir.ws>
2020-10-25 01:05:19 +0500

    Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,22 +63,35 @@ const getUserProfile = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin
     })
   } else {
-    res.status(401)
+    res.status(404)
     throw new Error('User not found')
   }
-
-  // if (user && (await user.matchPassword(password))) {
-  //   res.json({
-  //     _id: user._id,
-  //     name: user.name,
-  //     email: user.email,
-  //     isAdmin: user.isAdmin,
-  //     token: generateToken(user._id)
-  //   })
-  // } else {
-  //   res.status(401)
-  //   throw new Error('Invalid email or password')
-  // }
 })
 
-module.exports = { authUser, registerUser, getUserProfile }
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if(req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updateUser = await user.save()
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+      token: generateToken(updateUser._id)
+    })
+
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+module.exports = { authUser, registerUser, getUserProfile, updateUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const { authUser, registerUser, getUserProfile } = require('../controllers/userController')
+const { authUser, registerUser, getUserProfile, updateUserProfile } = require('../controllers/userController')
 const router = express.Router()
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 module.exports = router
